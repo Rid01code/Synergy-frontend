c4ff19036ea13ccb36e7a0e9d3f8e9b6456cf8ed
Ridwan Rahaman <rahamanridwan5@gmail.com>
2024-08-04 14:26:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,17 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
+const port = process.env.PORT || 3000
+
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(process.env.PORT || 3000, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:' + (process.env.PORT || 3000))
+    console.log(`> Ready on http://localhost:${port}`)
   })
+}).catch(err => {
+  console.error('Error occurred starting server:', err)
+  process.exit(1)
 })
