c6266fe7419b1d2f5b88659f12fa5c4cffd3a3a3
Ridwan <itsridOfficcial@gmail.com>
2024-08-04 13:23:33 +0530

    add .yml file

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 const nextConfig = {
   env: {
-    PORT_URL: process.env.PORT_URL || process.env['APPSETTING_PORT_URL'],
-    NEXT_PUBLIC_SOCKET_URL: process.env.NEXT_PUBLIC_SOCKET_URL || process.env['APPSETTING_NEXT_PUBLIC_SOCKET_URL'],
+    PORT_URL: process.env.PORT_URL 
 
   }
 };
